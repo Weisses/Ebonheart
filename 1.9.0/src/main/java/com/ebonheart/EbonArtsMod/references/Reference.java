ef851da8f1393b6fabb5be99f10fba8e15b44f37
Viesis <david.l.aiello@gmail.com>
2016-04-20 20:54:06 -0400

    Prerelease v2.1.6a
    Let's dooo ttthhhiiisss!

--- 1.9.0/src/main/java/com/ebonheart/EbonArtsMod/references/Reference.java
+++ 1.9.0/src/main/java/com/ebonheart/EbonArtsMod/references/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	//Main
 	public static final String MOD_ID = "ea";
 	public static final String MOD_NAME = "Ebon Arts";
-	public static final String MOD_VERSION = "v2.1.4a-mc1.9.0";
+	public static final String MOD_VERSION = "v2.1.6a-mc1.9.0";
 	public static final String CONFIG_GUI = "com.ebonheart.EbonArtsMod.api.EAGuiFactory";
 	public static final String CLIENT_PROXY_CLASS = "com.ebonheart.EbonArtsMod.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.ebonheart.EbonArtsMod.proxy.ServerProxy";
