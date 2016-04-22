8221f2ef352975d0bf9a9f19c47ef180516355b9
Viesis <david.l.aiello@gmail.com>
2016-04-22 07:36:02 -0400

    v2.1.8
    Void armor is working on telelporting.  Has cool particles trailing
    behind.  Still need to apply a cooldown, and another particle.
    
    NOTE TO SELF: add other particles to armor.

--- 1.9.0/src/main/java/com/ebonheart/EbonArtsMod/common/items/armor/ItemVoidArmor.java
+++ 1.9.0/src/main/java/com/ebonheart/EbonArtsMod/common/items/armor/ItemVoidArmor.java
@@ -1,6 +1,7 @@
 package com.ebonheart.EbonArtsMod.common.items.armor;
 
 import java.util.List;
+import java.util.Random;
 import java.util.Vector;
 
 import com.ebonheart.EbonArtsMod.EbonArtsMod;
@@ -10,6 +11,7 @@ import com.ebonheart.EbonArtsMod.common.items.ItemHelper;
 import com.ebonheart.EbonArtsMod.init.InitItemsEA;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.spectator.categories.TeleportToPlayer;
 import net.minecraft.client.renderer.Vector3d;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
@@ -27,12 +29,14 @@ import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.math.Vec4b;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.Teleporter;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -41,6 +45,9 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ItemVoidArmor extends ItemArmor {
 	
 	public static boolean isTeleported;
+	Random random = new Random();
+	public float width;
+	public float height;
 	
 	public ItemVoidArmor(String unlocalizedName, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn)
 	{
@@ -81,11 +88,12 @@ public class ItemVoidArmor extends ItemArmor {
 			&& player.getItemStackFromSlot(EntityEquipmentSlot.FEET) != null && player.getItemStackFromSlot(EntityEquipmentSlot.FEET).getItem() == InitItemsEA.void_boots) 
 		{
 			player.fallDistance = 0;
-			if(!player.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).hasModifier(EAAttributeModifier.SCALEMAIL_SPEED_BONUS))
-			{
-				player.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).applyModifier(EAAttributeModifier.SCALEMAIL_SPEED_BONUS);
-				player.fallDistance = 0;
-			}
+			
+			//if(!player.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).hasModifier(EAAttributeModifier.SCALEMAIL_SPEED_BONUS))
+			//{
+			//	player.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).applyModifier(EAAttributeModifier.SCALEMAIL_SPEED_BONUS);
+			//	player.fallDistance = 0;
+			//}
 			
 			if(player.onGround) 
 			{
@@ -99,67 +107,98 @@ public class ItemVoidArmor extends ItemArmor {
 					if(!player.onGround && !isTeleported)
 					{
 						int blockReachDistance = 10;
-						float partialTicks = 1.0F;
-						double posX = 0;
-						double posY = 0;
-						double posZ = 0;
-						
+						float partialTicks = 10.1F;
+						double posX;
+						double posY;
+						double posZ;
 						double playerPosX;
 						double playerPosY;
 						double playerPosZ;
+						int i = 128;
 						
-						double test1;
-						double test2;
-						double test3;
-						
-						//player.posX = posX;
-						//player.posY = posY;
-						//player.posZ = posZ;
-						
-						//;
 						playerPosX = player.getPosition().getX();
 						playerPosY = player.getPosition().getY();
 						playerPosZ = player.getPosition().getZ();
 						
-						
-						//player.rayTrace(blockReachDistance, partialTicks);
-						BlockPos pos = new BlockPos(player.rayTrace(10, 1).getBlockPos());
-						//BlockPos pos = new BlockPos(player.rayTrace(10, 1).sideHit.getDirectionVec());
-						//BlockPos pos = new BlockPos(player.rayTrace(10, 1).getBlockPos().getDistance(this.getX(), p_185332_2_, p_185332_3_));
-						
+						BlockPos pos = new BlockPos(player.rayTrace(10, partialTicks).getBlockPos());
 						posX = pos.getX();
 						posY = pos.getY();
 						posZ = pos.getZ();
 						
 						
-						//if (//playerPosX + 10 <= posX
+						player.teleportTo_(posX, posY, posZ);
+						LogHelper.info("Teleport SUCCESS!");
+						LogHelper.info("Player  Coors:" +  "X: " + playerPosX + " | Y: " + playerPosY + " | Z: " + playerPosZ);
+						LogHelper.info("Raycast Coors:" +  pos);
+							
+						isTeleported = true;
+
+							
+						for (int j = 0; j < i; ++j)
+				        {
+							double d6 = (double)j / ((double)i - 1.0D);
+							float f = (random.nextFloat() - 0.5F) * 0.2F;
+			                float f1 = (random.nextFloat() - 0.5F) * 0.2F;
+			                float f2 = (random.nextFloat() - 0.5F) * 0.2F;
+			                double d3 = playerPosX + (playerPosX - playerPosX) * d6 + (random.nextDouble() - 0.5D) * (double)this.width * 2.0D;
+			                double d4 = playerPosY + (playerPosY - playerPosY) * d6 + random.nextDouble() * (double)this.height;
+			                double d5 = playerPosZ + (playerPosZ - playerPosZ) * d6 + (random.nextDouble() - 0.5D) * (double)this.width * 2.0D;
+			                
+			                world.spawnParticle(EnumParticleTypes.PORTAL, d3, d4, d5, (double)f, (double)f1, (double)f2, new int[0]);
+						
+				        }
+							
+						player.moveToBlockPosAndAngles(pos, player.getRotationYawHead(), 1);
+							
+					}
+				}
+			}
+		}
+		else
+		{
+			player.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).removeModifier(EAAttributeModifier.SCALEMAIL_SPEED_BONUS);
+		}
+	}
+}
+
+/**
+//player.posX = posX;
+						//player.posY = posY;
+						//player.posZ = posZ;
+						
+						//;
+						 * //player.getTeleportDirection();
+						//player.rayTrace(blockReachDistance, partialTicks);
+						//BlockPos pos = new BlockPos(player.rayTrace(10, partialTicks).hitVec);
+						
+						 
+
+//BlockPos pos = new BlockPos(player.rayTrace(10, 1).sideHit.getDirectionVec());
+//BlockPos pos = new BlockPos(player.rayTrace(10, 1).getBlockPos().getDistance(this.getX(), p_185332_2_, p_185332_3_));
+//if (//playerPosX + 10 <= posX
 						//		posX != null
 								//posX <= playerPosX + 10 || posX >= playerPosX - 10
 						 //&& posY <= playerPosY + 10 || posY >= playerPosY - 10
 						 //&& posZ <= playerPosZ + 10 || posZ >= playerPosZ - 10
 						//		)
 						//{
-						/**
+						
 						//test = (posD - posY);
 						//test1 = posX >= 10 || posX <= -10; 
 						if (posX <= playerPosX + 10 || posX >= playerPosX - 10
 						 && posY <= playerPosY + 10 || posY >= playerPosY - 10
 						 && posZ <= playerPosZ + 10 || posZ >= playerPosZ - 10)
 						{
-						**/
-						
-						player.addVelocity((posX - playerPosX) * 0.1, (posY - playerPosY) * 0.1, (posZ - playerPosZ) * 0.1);
 						
 						
+						//player.addVelocity((posX - playerPosX) * 0.1, (posY - playerPosY) * 0.1, (posZ - playerPosZ) * 0.1);
+						//test3 = player.playerLocation.offset(player.getHorizontalFacing(), 10);
+						//player.teleportTo_(posX, 
+						//		posY, 
+						//		posZ);
+						//test2 = player.getTeleportDirection().getDirectionVec().getY();
 						//player.addVelocity((posX - playerPosX), (posY - playerPosY), (posZ - playerPosZ));
-							//player.teleportTo_(posX, posY, posZ);
-							LogHelper.info("Teleport SUCCESS!");
-							LogHelper.info("Player  Coors:" +  "X: " + playerPosX + " | Y: " + playerPosY + " | Z: " + playerPosZ);
-							LogHelper.info("Raycast Coors:" +  "X: " + posX + " | Y: " + posY + " | Z: " + posZ);
-							
-							isTeleported = true;
-							
-							
+						//player.getRotatedYaw(null)
 						//}
 						//else
 						//{
@@ -179,13 +218,7 @@ public class ItemVoidArmor extends ItemArmor {
 						//player.teleportTo_(posX, posY, posZ);
 						//isTeleported = true;
 						//LogHelper.info("Coors:" +  "X: " + posX + " | Y: " + pos.getY() + " | Z: " + posZ);
-					}
-				}
-			}
-		}
-		else
-		{
-			player.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).removeModifier(EAAttributeModifier.SCALEMAIL_SPEED_BONUS);
-		}
-	}
-}
+					
+						
+
+**/
